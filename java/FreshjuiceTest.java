7d4371c54f10368be217dc94663d2da31c235b08
joshuanixi <amazingworkjoshua@gmail.com>
2016-07-13 14:10:40 -0700

    add jave code

--- java/FreshjuiceTest.java
+++ java/FreshjuiceTest.java
@@ -12,6 +12,74 @@ class FreshJuice{
 	FreshJuiceSize size;
 }
 
+class FatherClass{
+
+
+    FatherClass(){
+    	System.out.println("Father construction method");
+    }
+	public int value;
+	public void f(){
+		value=100;
+		System.out.println("Fatherclass.value:"+value);
+
+
+	}
+}
+
+class ChildClass extends FatherClass{
+
+	ChildClass(){
+    	System.out.println("Child construction method");
+    }
+
+	public int value;
+	public void f(){
+
+		super.f();
+		value=200;
+		System.out.println("ChildClass.value:"+value);
+		System.out.println(value);
+		System.out.println(super.value);
+	}
+
+
+
+
+}
+
+class BirthDate{
+	private int day;
+	private int month;
+	private int year;
+	public BirthDate(int d, int m,int y){
+		day=d;
+		month=m;
+		year=y;
+	}
+
+	public void setDay(int d) {day=d;}
+	public void setMonth(int m){month=m;}
+	public void setyear(int y){year=y;}
+
+	public int getDay(){return day;}
+	public int getMonth(){return month;}
+	public int getYear(){return year;}
+
+	public void display(){
+
+		System.out.println("Your birthday is "+day+"/"+month+"/"+year);
+	}
+
+
+
+
+
+}
+
+
+
+
 public class FreshJuiceTest{
 
 	public static int methon(int n){
@@ -46,6 +114,16 @@ public class FreshJuiceTest{
 		System.out.println(Fibonacci(3));
 		System.out.println(Fibonacci(4));
 		System.out.println(Fibonacci(10));
+
+
+		BirthDate birthdate=new BirthDate(8,01,1988);
+        birthdate.display();
+
+
+        ChildClass child=new ChildClass();
+        child.f();
+
+
 
 	}
 }
